8d47f796a9f24e5b1cabe0295bbd165b4c8ee1e5
edu macias <edu.maciass@ug.edu.ec>
2018-06-10 15:36:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/arist/curriculumedu/referenciaspersonales.java
@@ -0,0 +1,16 @@
+package com.example.arist.curriculumedu;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class referenciaspersonales extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_referncias_personales, container, false);
+    }
+}
